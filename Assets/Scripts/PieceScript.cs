00301eef92e4e4cafbe941ef87531ecc25a82a8c
spamspambot <spambot@outlook.dk>
2018-11-15 10:23:08 +0100

    213

---
+++ Assets/Scripts/PieceScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceScript : MonoBehaviour {
+    public GameObject evolutionTarget;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Piece")) {
+            Instantiate(evolutionTarget, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
